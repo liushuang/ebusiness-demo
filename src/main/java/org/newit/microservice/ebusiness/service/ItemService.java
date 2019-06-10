57e77c536171190141adf422f3caf4fb8f060159
liushuang <liu.shuang@linecorp.com>
2019-06-10 20:27:41 +0800

    + add item service

--- src/main/java/org/newit/microservice/ebusiness/service/ItemService.java
+++ src/main/java/org/newit/microservice/ebusiness/service/ItemService.java
@@ -3,25 +3,28 @@ package org.newit.microservice.ebusiness.service;
 import java.util.List;
 
 import org.newit.microservice.ebusiness.model.Item;
-import org.newit.microservice.ebusiness.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.alibaba.fastjson.JSONObject;
 
 @Service
 public class ItemService {
 
     @Autowired
-    private ItemRepository itemRepository;
+    private RestTemplate restTemplate;
 
     public Item getItemById(long itemId) {
-        return itemRepository.getItemById(itemId);
+        Item item = restTemplate.getForObject("http://localhost:29610/item/" + itemId, Item.class);
+        return item;
     }
 
     public void insert(Item item) {
-        itemRepository.insert(item);
+        restTemplate.postForObject("http://localhost:29610/item/insert", item, JSONObject.class);
     }
 
     public List<Item> getItemAllList() {
-        return itemRepository.getItemAllList();
+        return restTemplate.getForObject("http://localhost:29610/item/allList", List.class);
     }
 }
